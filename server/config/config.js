bce51194c951fe3f2f7fccd908c2f11d1a15aa17
Matias <mparodi@evosoftuy.com>
2018-08-16 09:39:38 -0300

    Variable de entorno connection string

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb://cafe-user:123456P@ds121262.mlab.com:21262/cafe';
+    urlDB = procss.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
